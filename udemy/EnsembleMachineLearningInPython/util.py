949e5e98b351aa5479e84c97b71d70ebf02619ee
johnklee <puremonkey2001@yahoo.com.tw>
2020-07-14 11:03:08 +0800

    Partial section4 course materials

--- udemy/EnsembleMachineLearningInPython/util.py
+++ udemy/EnsembleMachineLearningInPython/util.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.tree import DecisionTreeRegressor, DecisionTreeClassifier
+
 
 def plot_decision_boundary(ax, X, model):
     h = .02  # step size in the mesh
@@ -17,3 +19,71 @@ def plot_decision_boundary(ax, X, model):
     # Put the result into a color plot
     Z = Z.reshape(xx.shape)
     ax.contour(xx, yy, Z, cmap=plt.cm.Paired)
+
+
+class BaggedTreeRegressor:
+    def __init__(self, n_estimators):
+        '''
+        @param B(int):
+            Sampling time
+        '''
+        self.B = n_estimators
+
+    def fit(self, X, Y):
+        N = len(X)
+        self.models = []
+        for b in range(self.B):
+            idx = np.random.choice(N, size=N, replace=True)
+            Xb = X[idx]
+            Yb = Y[idx]
+
+            model = DecisionTreeRegressor()
+            model.fit(Xb, Yb)
+            self.models.append(model)
+
+    def predict(self, X):
+        predictions = np.zeros(len(X))
+        for model in self.models:
+            predictions += model.predict(X)
+            
+        return predictions / self.B
+
+    def score(self, X, Y):
+        d1 = Y - self.predict(X)
+        d2 = Y - Y.mean()
+        return 1 - d1.dot(d1) / d2.dot(d2)
+
+
+class BaggedTreeClassifier:
+    def __init__(self, B, max_depth=2):
+        r'''
+        @param B:
+            Number of sampling in building bagged dt classifier
+        @param max_depth:
+            Hyperparameter `max_depth` of DecisionTreeClassifier
+        '''
+        self.B = B
+        self.max_depth = max_depth
+
+    def fit(self, X, Y):
+        N = len(X)
+        self.models = []
+        for b in range(self.B):
+            idx = np.random.choice(N, size=N, replace=True)
+            Xb = X[idx]
+            Yb = Y[idx]
+
+            model = DecisionTreeClassifier(max_depth=self.max_depth)
+            model.fit(Xb, Yb)
+            self.models.append(model)
+
+    def predict(self, X):
+        # no need to keep a dictionary since we are doing binary classification
+        predictions = np.zeros(len(X))
+        for model in self.models:
+            predictions += model.predict(X)
+        return np.round(predictions / self.B)
+
+    def score(self, X, Y):
+        P = self.predict(X)
+        return np.mean(Y == P)
