4f2b3c060c08897aa51c764969d3f246c0327a44
johnklee <puremonkey2001@yahoo.com.tw>
2020-07-15 10:18:34 +0800

    Completed section4 course materials and initialize section5 notebook

--- udemy/EnsembleMachineLearningInPython/util.py
+++ udemy/EnsembleMachineLearningInPython/util.py
@@ -55,14 +55,14 @@ class BaggedTreeRegressor:
 
 
 class BaggedTreeClassifier:
-    def __init__(self, B, max_depth=2):
+    def __init__(self, n_estimators, max_depth=2):
         r'''
         @param B:
             Number of sampling in building bagged dt classifier
         @param max_depth:
             Hyperparameter `max_depth` of DecisionTreeClassifier
         '''
-        self.B = B
+        self.B = n_estimators
         self.max_depth = max_depth
 
     def fit(self, X, Y):
@@ -87,3 +87,35 @@ class BaggedTreeClassifier:
     def score(self, X, Y):
         P = self.predict(X)
         return np.mean(Y == P)
+
+
+def replace_missing(df):
+    # standard method of replacement for numerical columns is median
+    for col in NUMERICAL_COLS:
+        if np.any(df[col].isnull()):
+            med = np.median(df[ col ][ df[col].notnull() ])
+            df.loc[ df[col].isnull(), col ] = med
+
+    # set a special value = 'missing'
+    for col in CATEGORICAL_COLS:
+        if np.any(df[col].isnull()):
+            print(col)
+            df.loc[ df[col].isnull(), col ] = 'missing'
+            
+
+def get_mushroom_data():
+    df = pd.read_csv('datas/agaricus-lepiota.data', header=None)
+
+    # replace label column: e/p --> 0/1
+    # e = edible = 0, p = poisonous = 1
+    df[0] = df.apply(lambda row: 0 if row[0] == 'e' else 1, axis=1)
+
+    # check if there is missing data
+    replace_missing(df)
+
+    # transform the data
+    transformer = DataTransformer()
+
+    X = transformer.fit_transform(df)
+    Y = df[0].values
+    return X, Y
