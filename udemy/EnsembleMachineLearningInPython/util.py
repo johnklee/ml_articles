6344ccd5d53275caf2c12d19493042f31d022233
johnklee <puremonkey2001@yahoo.com.tw>
2020-07-16 10:57:01 +0800

    Add section5 course materials

--- udemy/EnsembleMachineLearningInPython/util.py
+++ udemy/EnsembleMachineLearningInPython/util.py
@@ -1,6 +1,72 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeRegressor, DecisionTreeClassifier
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+
+
+MUSHROOM_DATA_NUMERICAL_COLS = [
+  'crim', # numerical
+  'zn', # numerical
+  'nonretail', # numerical
+  'nox', # numerical
+  'rooms', # numerical
+  'age', # numerical
+  'dis', # numerical
+  'rad', # numerical
+  'tax', # numerical
+  'ptratio', # numerical
+  'b', # numerical
+  'lstat', # numerical
+]
+
+MUSHROOM_DATA_NO_TRANSFORM = ['river']
+
+MUSHROOM_DATA_NUMERICAL_COLS = ()
+MUSHROOM_DATA_CATEGORICAL_COLS = np.arange(22) + 1 # 1..22 inclusive
+
+class MushroomDataTransformer:
+    def fit(self, df):
+        self.labelEncoders = {}
+        self.scalers = {}
+        for col in MUSHROOM_DATA_NUMERICAL_COLS:
+            scaler = StandardScaler()
+            scaler.fit(df[col].reshape(-1, 1))
+            self.scalers[col] = scaler
+
+        for col in MUSHROOM_DATA_CATEGORICAL_COLS:
+            encoder = LabelEncoder()
+            # in case the train set does not have 'missing' value but test set does
+            values = df[col].tolist()
+            values.append('missing')
+            encoder.fit(values)
+            self.labelEncoders[col] = encoder
+
+        # find dimensionality
+        self.D = len(MUSHROOM_DATA_NUMERICAL_COLS)
+        for col, encoder in self.labelEncoders.items():
+            self.D += len(encoder.classes_)
+            
+        print(f"dimensionality: {self.D}")
+
+    def transform(self, df):
+        N, _ = df.shape
+        X = np.zeros((N, self.D))
+        i = 0
+        for col, scaler in self.scalers.items():
+            X[:,i] = scaler.transform(df[col].values.reshape(-1, 1)).flatten()
+            i += 1
+
+        for col, encoder in self.labelEncoders.items():
+            # print "transforming col:", col
+            K = len(encoder.classes_)
+            X[np.arange(N), encoder.transform(df[col]) + i] = 1
+            i += K
+        return X
+
+    def fit_transform(self, df):
+        self.fit(df)
+        return self.transform(df)
 
 
 def plot_decision_boundary(ax, X, model):
@@ -91,13 +157,13 @@ class BaggedTreeClassifier:
 
 def replace_missing(df):
     # standard method of replacement for numerical columns is median
-    for col in NUMERICAL_COLS:
+    for col in MUSHROOM_DATA_NUMERICAL_COLS:
         if np.any(df[col].isnull()):
             med = np.median(df[ col ][ df[col].notnull() ])
             df.loc[ df[col].isnull(), col ] = med
 
     # set a special value = 'missing'
-    for col in CATEGORICAL_COLS:
+    for col in MUSHROOM_DATA_CATEGORICAL_COLS:
         if np.any(df[col].isnull()):
             print(col)
             df.loc[ df[col].isnull(), col ] = 'missing'
@@ -114,7 +180,7 @@ def get_mushroom_data():
     replace_missing(df)
 
     # transform the data
-    transformer = DataTransformer()
+    transformer = MushroomDataTransformer()
 
     X = transformer.fit_transform(df)
     Y = df[0].values
