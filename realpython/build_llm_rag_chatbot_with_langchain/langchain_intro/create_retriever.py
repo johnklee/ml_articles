10eb4a0321a7b40794e485c07a1e3def98ca6e28
John Lee <johnkclee@google.com>
2024-04-21 03:10:21 +0000

    add partial article `build_llm_rag_chatbot_with_langchain`

---
+++ realpython/build_llm_rag_chatbot_with_langchain/langchain_intro/create_retriever.py
@@ -0,0 +1,18 @@
+"""Build up Chroma DB for RAG for reviews from `data/reviews.csv`."""
+import dotenv
+from langchain.document_loaders.csv_loader import CSVLoader
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+
+
+REVIEWS_CSV_PATH = "data/reviews.csv"
+REVIEWS_CHROMA_PATH = "chroma_data"
+
+dotenv.load_dotenv()
+
+loader = CSVLoader(file_path=REVIEWS_CSV_PATH, source_column="review")
+reviews = loader.load()
+
+reviews_vector_db = Chroma.from_documents(
+    reviews, OpenAIEmbeddings(), persist_directory=REVIEWS_CHROMA_PATH
+)
