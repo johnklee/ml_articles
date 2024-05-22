e401068f362467b524cd6ec881d087a49a59405e
John Lee <johnkclee@google.com>
2024-05-22 14:07:03 +0000

    update notebook and utilities for LLM+RAG demo

--- others/Chroma_usage_demo/ingest_bttc_repo.py
+++ others/Chroma_usage_demo/ingest_bttc_repo.py
@@ -8,6 +8,67 @@ from chromadb.utils import embedding_functions
 ENV_REPO_PATH = 'BTTC_REPO_ROOT'
 
 
+def ingestion_of_repo_dataset_gen_with_split(
+    default_repo_path: str = None, max_line_num: int = 50,
+    context_line_num: int = 5):
+  """Generates dataset of repo for ingestion with split actions."""
+  ids = []
+  documents = []
+  metadatas = []
+
+  id_current_num = 1
+  repo_path = os.environ.get(ENV_REPO_PATH, default_repo_path)
+  if repo_path is None:
+    raise Exception(
+        f'Environment variable="{ENV_REPO_PATH}" does not exist!')
+
+  if repo_path is None:
+    raise Exception(
+        f'Environment variable="{ENV_REPO_PATH}" does not exist!')
+
+  for root, dirs, files in os.walk(repo_path):
+    for file in files:
+      file_extension = file.split('.')[-1]
+      if any([
+          file_extension not in {'py', 'md', 'txt'},
+          file.startswith('.')]):
+        continue
+
+      file_path = os.path.join(root, file)
+      lines = open(file_path).read().split('\n')
+      current_doc_lines = []
+      for start_line_num in range(0, len(lines), 50):
+        if not current_doc_lines:
+          current_doc_lines = lines[start_line_num:start_line_num+max_line_num]
+        else:
+          current_doc_lines = (
+              current_doc_lines[-context_line_num:] +
+              lines[start_line_num:start_line_num+max_line_num])
+
+        id_current_num += 1
+        ids.append(str(id_current_num))
+        documents.append('\n'.join(current_doc_lines))
+        metadatas.append({
+            'file_path': file_path,
+            'file_type': file_extension,
+        })
+        if len(ids) == 10:
+          yield {
+              "ids": ids,
+              "documents": documents,
+              "metadatas": metadatas}
+
+          ids = []
+          documents = []
+          metadatas = []
+
+      if ids:
+        yield {
+            "ids": ids,
+            "documents": documents,
+            "metadatas": metadatas}
+
+
 def ingestion_of_repo_dataset_gen(default_repo_path: str = None):
   """Generates dataset of repo for ingestion."""
   ids = []
@@ -64,7 +125,7 @@ def build_chroma_collection(
   for collection in chroma_client.list_collections():
     if collection.name == collection_name:
       print(f'Collect with name="{collection_name}" has been created!')
-      return
+      return collection
 
   embedding_func = embedding_functions.SentenceTransformerEmbeddingFunction(
       model_name=embedding_func_name)
@@ -75,7 +136,7 @@ def build_chroma_collection(
       metadata={"hnsw:space": distance_func_name})
 
   ingest_doc_count = 0
-  for dataset in ingestion_of_repo_dataset_gen(default_repo_path):
+  for dataset in ingestion_of_repo_dataset_gen_with_split(default_repo_path):
     collection.add(
         ids=dataset['ids'],
         documents=dataset['documents'],
--- others/Chroma_usage_demo/llm_utils.py
+++ others/Chroma_usage_demo/llm_utils.py
@@ -1,7 +1,9 @@
 """Module to obtain LLM agent for quick evaluation of LLM."""
+import dataclasses
 import os
 import json
 import openai
+from typing import Any
 
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
@@ -10,12 +12,27 @@ with open("config.json", mode="r") as json_file:
     config_data = json.load(json_file)
 
 
+@dataclasses.dataclass
+class FullResp:
+  context: str
+  question: str
+  response: Any
+
+
 class LLMAgent:
   """LLM agent for simple chat interaction."""
+
+  DEFAULT_RAG_CONTEXT_FORMAT = """
+You are a software engineer with less experience in using bttc.
+Use the following collected information to answer questions: {}
+"""
+  DEFAULT_RAG_QUESTION_FORMAT = '{}'
 
   def __init__(
       self, config_data: dict[str, str] | None = None,
       context: str = 'You are a software engineer.',
+      rag_context_format: str | None = None,
+      rag_question_format: str | None = None,
       model_name: str = 'gpt-3.5-turbo'):
     if config_data is None:
       with open('config.json', mode='r') as json_file:
@@ -27,6 +44,10 @@ class LLMAgent:
         api_key=config_data.get('openai-secret-key'))
     self.context = context
     self.model_name = 'gpt-3.5-turbo'
+    self.rag_context_format = (
+        rag_context_format or self.DEFAULT_RAG_CONTEXT_FORMAT)
+    self.rag_question_format = (
+        rag_question_format or self.DEFAULT_RAG_QUESTION_FORMAT)
 
   @property
   def client(self):
@@ -38,6 +59,7 @@ class LLMAgent:
       model_name: str | None = None,
       context: str | None = None,
       temperature: int = 0) -> str:
+    """Answers the input question by LLM."""
     model = model_name or self.model_name
     context = context or self.context
     chat_completion = self.client.chat.completions.create(
@@ -51,3 +73,34 @@ class LLMAgent:
 
     return chat_completion.choices[0].message.content
 
+  def answer_with_rag(
+      self,
+      collection: Any,
+      question: str,
+      model_name: str | None = None,
+      context: str | None = None,
+      temperature: int = 0,
+      rag_max_doc_count: int = 10,
+      return_full: bool = False) -> str:
+    """Answer the given question with help of LLM + RAG."""
+    related_docs = collection.query(
+        query_texts=[question],
+        n_results=rag_max_doc_count,
+        include=["documents"])
+
+    docs_str = ",".join(related_docs["documents"][0])
+    context = self.rag_context_format.format(docs_str)
+    question = self.rag_question_format.format(question)
+    resp = self.answer(
+        question=question,
+        context=context,
+        model_name=model_name,
+        temperature=temperature)
+
+    if return_full:
+      return FullResp(
+          context=context,
+          question=question,
+          response=resp)
+
+    return resp
