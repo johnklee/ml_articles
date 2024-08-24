c89236b6a19d65760d2e096beba8402b16fbb9c4
John Lee <johnkclee@google.com>
2024-08-24 09:21:18 +0000

    add new notebook

--- google/gemini/cookbook/config_genai.py
+++ google/gemini/cookbook/config_genai.py
@@ -17,4 +17,7 @@ def get_model(model_name: str = 'gemini-1.5-flash'):
   Returns:
     The Genai model object.
   """
-  return genai.GenerativeModel(f'models/{model_name}')
+  if not model_name.startswith('models/'):
+    model_name = f'models/{model_name}'
+
+  return genai.GenerativeModel(model_name)
