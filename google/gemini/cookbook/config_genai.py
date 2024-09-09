cf3507a07cba8a4be1c1da5a30a9e76077a7fed5
John Lee <johnkclee@google.com>
2024-09-09 11:46:54 +0000

    Add new notebooks

--- google/gemini/cookbook/config_genai.py
+++ google/gemini/cookbook/config_genai.py
@@ -3,13 +3,16 @@
 import google.generativeai as genai
 import os
 from dotenv import load_dotenv, find_dotenv
+from typing import Any
 
 
 _ = load_dotenv(find_dotenv(os.path.expanduser('~/.env'))) # read local .env file
 genai.configure(api_key=os.environ['GOOGLE_API_KEY'])
 
 
-def get_model(model_name: str = 'gemini-1.5-flash'):
+def get_model(
+    model_name: str = 'gemini-1.5-flash',
+    generation_config: dict[str, Any] | None = None):
   """Obtains model by the given name.
   Args:
     model_name: Model name.
@@ -19,5 +22,9 @@ def get_model(model_name: str = 'gemini-1.5-flash'):
   """
   if not model_name.startswith('models/'):
     model_name = f'models/{model_name}'
+
+  if generation_config:
+    return genai.GenerativeModel(
+        model_name, generation_config=generation_config)
 
   return genai.GenerativeModel(model_name)
---
+++ google/gemini/cookbook/utils.py
@@ -0,0 +1,7 @@
+"""Utilities used in the notebook."""
+
+from IPython.display import display, Markdown
+
+
+def print_markdown(text):
+  display(Markdown(text))
